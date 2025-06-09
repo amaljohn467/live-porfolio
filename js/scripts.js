7edf381bf39b547ccd83a7e1760247a865a33c35
amaljohn467 <amaljohn467@gmail.com>
2025-06-09 12:19:04 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed('#typed-text', {
+    strings: ["Developer", "Designer", "Freelancer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+  });
+});
+
